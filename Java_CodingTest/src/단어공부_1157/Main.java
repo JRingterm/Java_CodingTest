d9443c2c3e43f926723f6e8a377f7e557ea7de7b
JRingterm <ghwns3712@naver.com>
2025-02-16 20:46:52 +0900

    [feat] 단어 공부_1157

---
+++ Java_CodingTest/src/단어공부_1157/Main.java
@@ -0,0 +1,42 @@
+package 단어공부_1157;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        String trans = word.toUpperCase();
+
+        int [] count = new int[26];
+
+        for(int i = 0; i<trans.length(); i++){
+            char ch = trans.charAt(i);
+            count[ch - 'A']++;
+        }
+        int max = 0;
+        char alpha = ' ';
+
+        for(int i = 0; i<26; i++){
+            if(count[i]>max){
+                max = count[i];
+            }
+        }
+
+        int maxCount = 0;
+        for(int i = 0; i<26; i++){
+            if(count[i] == max){
+                maxCount++;
+                alpha = (char)(i + 'A');
+            }
+        }
+        if(maxCount != 1){
+            System.out.println('?');
+        }
+        else{
+            System.out.println(alpha);
+        }
+    }
+}

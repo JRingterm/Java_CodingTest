5581193f45f5565dcf2b16fa448834efa43fb2e7
JRingterm <ghwns3712@naver.com>
2025-05-19 18:26:15 +0900

    [feat] 벌집 2292 풀이

---
+++ Java_CodingTest/src/벌집_2292/Main.java
@@ -0,0 +1,24 @@
+package 벌집_2292;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int step = 1; //벌집 한겹에서의 최대 번호
+        int count = 1; //N까지 도달하기 위한 방의 수
+
+        while(true){
+            step += 6*(count-1);
+            if(N<=step){
+                System.out.println(count);
+                break;
+            }
+            count += 1;
+        }
+    }
+}

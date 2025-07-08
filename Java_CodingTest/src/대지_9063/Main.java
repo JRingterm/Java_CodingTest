16d4c7316daf5d14868709ffe097b0402f511dce
JRingterm <ghwns3712@naver.com>
2025-07-08 18:26:07 +0900

    [feat] 대지_9063 풀이

---
+++ Java_CodingTest/src/대지_9063/Main.java
@@ -0,0 +1,28 @@
+package 대지_9063;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> x = new ArrayList<Integer>();
+        List<Integer> y = new ArrayList<Integer>();
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x.add(Integer.parseInt(st.nextToken()));
+            y.add(Integer.parseInt(st.nextToken()));
+        }
+
+        //최대, 최소의 x,y값만 알면 됨.
+        int result =(Collections.max(x) - Collections.min(x)) * (Collections.max(y) - Collections.min(y));
+        System.out.println(result);
+    }
+}
